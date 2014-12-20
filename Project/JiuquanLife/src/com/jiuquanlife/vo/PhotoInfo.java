b28ef58ec14ad2f80f40a25214247bdc54cb65b1
NeilZky <zhangky1989@126.com>
2014-12-20 19:16:31 +0800

    点击轮播图片进入帖子详情功能

--- Project/JiuquanLife/src/com/jiuquanlife/vo/PhotoInfo.java
+++ Project/JiuquanLife/src/com/jiuquanlife/vo/PhotoInfo.java
@@ -2,12 +2,13 @@ package com.jiuquanlife.vo;
 
 public class PhotoInfo {
 	
-	public String id;
+	public String tid;
 	public String url;
 	public String title;
 	
-	public PhotoInfo(String id, String url, String title) {
+	public PhotoInfo(String tid, String url, String title) {
 		
+		this.tid = tid;
 		this.url = url;
 		this.title = title;
 	}
