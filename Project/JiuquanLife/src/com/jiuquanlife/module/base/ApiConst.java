b1fd9811df6c9af54dbc75b57d7cb18556fa0a48
StevenShe <shechaohai1@163.com>
2014-12-21 15:07:24 +0800

    add apiconst

---
+++ Project/JiuquanLife/src/com/jiuquanlife/module/base/ApiConst.java
@@ -0,0 +1,12 @@
+package com.jiuquanlife.module.base;
+
+public class ApiConst {
+	
+	private static String BASE_URL = "http://www.5ijq.cn";
+	//论坛页面板块
+	public static String GET_FORUM_LIST = BASE_URL + "/App/Index/getForumList";
+	//论坛页面新帖
+	public static String GET_NEW_FORUM_LIST = BASE_URL + "/App/Index/getNewList";
+
+	public static String GET_DIGEST_FORUM_LIST = BASE_URL + "/App/Index/getDigestList";
+}
